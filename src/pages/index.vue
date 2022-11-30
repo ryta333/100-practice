0fcff7ab9ebf2b5b09fa180e971325c90bf27065
ryta <Huang_yiho@hotmail.com>
2022-11-30 16:36:24 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+
+// const name = $ref('')
+
+// const router = useRouter()
+// const go = () => {
+//   if (name)
+//     router.push(`/hi/${encodeURIComponent(name)}`)
+// }
+</script>
+
+<template>
+  <div>
+    100 days pratice
+  </div>
+</template>
